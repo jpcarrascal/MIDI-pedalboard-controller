7b379a36ee0fbf3c90058e37c76d039227eab63b
JP Carrascal <juanpcarrascal@gmail.com>
2020-12-17 11:05:54 +0100

    Now supporting 4 knobs, some minor fixes

--- MIDI-pedalboard-controller.ino
+++ MIDI-pedalboard-controller.ino
@@ -8,9 +8,12 @@ const bool debug = false;
 PushButton sw_left = PushButton(2, ENABLE_INTERNAL_PULLUP);
 PushButton sw_right = PushButton(3, ENABLE_INTERNAL_PULLUP);
 PushButton sw_center = PushButton(4, ENABLE_INTERNAL_PULLUP);
-const int pot1 = A1;
+/*const int pot1 = A1;
 const int pot2 = A0;
-int pot1val, pot2val;
+const int pot3 = A2;
+const int pot4 = A3;*/
+const int pot[4] = {A1, A3, A2, A0};
+int potval[4]; //pot1val, pot2val;
 
 const int ledPin =  9;
 const long interval = 300; // blink interval
@@ -23,8 +26,13 @@ String mode = "CC"; // "CC" = Control Change, PC" = Program Change
 const int cc_left   = 0;
 const int cc_right  = 1;
 const int cc_center = 2;
+/*
 const int cc_pot1   = 3;
 const int cc_pot2   = 4;
+const int cc_pot3   = 5;
+const int cc_pot4   = 6;
+*/
+const int cc_pot[4] = {3, 4, 5, 6};
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 void setup() {
@@ -35,8 +43,9 @@ void setup() {
           Serial.begin(31250);
         pinMode(ledPin, OUTPUT);
         digitalWrite(ledPin, HIGH);
-        pot1val = analogRead(pot1);
-        pot2val = analogRead(pot2);
+        for(int i=0; i<4; i++) {
+          potval[i] = analogRead(pot[i]);
+        }
 
         // Footswitch press and release callbacks
         sw_left.onPress(onButtonPressed);
@@ -59,25 +68,25 @@ void loop() {
   if(mode == "PC") {
     if (currentMillis - previousMillis >= interval) {
       previousMillis = currentMillis;
-      ledState = !ledState;
+      ledState = (!ledState);
       digitalWrite(ledPin, ledState);
     } 
   }
   else // CC mode
     digitalWrite(ledPin, HIGH);
 
-  int pot1valnew = analogRead(pot1);
-  int pot2valnew = analogRead(pot2);
-  debugControl("pot 1", pot1valnew);
-  if( abs(pot1val - pot1valnew) > 7 && mode == "CC") {
-    int outval = mapAndClamp(pot1valnew);
-    MIDI.sendControlChange(cc_pot1, outval, CCchannel);
-    pot1val = pot1valnew;
+  int potvalNew[4];
+  for(int i=0; i<4; i++) {
+    potvalNew[i] = analogRead(pot[i]);
   }
-  if( abs(pot2val - pot2valnew) > 7 && mode == "CC") {
-    int outval = mapAndClamp(pot2valnew);
-    MIDI.sendControlChange(cc_pot2, outval, CCchannel);
-    pot2val = pot2valnew;
+  
+  for(int i=0; i<4; i++) {
+    if( abs(potval[i] - potvalNew[i]) > 7 && mode == "CC") {
+      int outval = mapAndClamp(potval[i]);
+      MIDI.sendControlChange(cc_pot[i], outval, CCchannel);
+      potval[i] = potvalNew[i];
+      debugControl("pot", i, potvalNew[i]);
+    }
   }
 }
 
@@ -136,22 +145,22 @@ void setPCmode() {
   mode = "PC";
 }
 
-void debugControl(String n, int v) {
+void debugControl(String name, int i, int value) {
   if (debug) {
-    Serial.print(n);
-    Serial.print(":\t");
-    Serial.println(v); 
+    Serial.print(name);
+    Serial.print("[");
+    Serial.print(i);
+    Serial.print("]");
+    Serial.print(": ");
+    Serial.print(value);
+    Serial.print("\tmapped: ");
+    Serial.println(mapAndClamp(value)); 
   }
 }
 
 int mapAndClamp(int input) {
-  if (debug) {
-    Serial.print("input: ");
-    Serial.println(input);
-  }
   int outval = map(input, 5, 1015, 0, 127);
   if(outval < 0) outval = 0;
   if(outval>127) outval = 127;
   return (outval);
 }
-
