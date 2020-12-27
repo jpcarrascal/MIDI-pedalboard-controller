6ea5b721035d2d995f4a5646354f534b9391a446
JP Carrascal <juanpcarrascal@gmail.com>
2020-12-27 09:52:33 +0100

    Added MIDI in and external pedal loop

--- MIDI-pedalboard-controller.ino
+++ MIDI-pedalboard-controller.ino
@@ -3,77 +3,110 @@
 #include <PushButton.h>
 #include <Bounce2.h>
 #include <MIDI.h>
+#include "MIDIUSB.h"
+
+#define POT_COUNT 4
 
 const bool debug = false;
+const bool usbMIDI = true; // Send MIDI via USB?
+const bool srlMIDI = true; // Send/receive MIDI via MIDI ports?
+const bool pickUpMode = true;
+const bool loopInternal = true;
 PushButton sw_left = PushButton(2, ENABLE_INTERNAL_PULLUP);
-PushButton sw_right = PushButton(3, ENABLE_INTERNAL_PULLUP);
-PushButton sw_center = PushButton(4, ENABLE_INTERNAL_PULLUP);
-/*const int pot1 = A1;
-const int pot2 = A0;
-const int pot3 = A2;
-const int pot4 = A3;*/
-const int pot[4] = {A1, A3, A2, A0};
-int potval[4]; //pot1val, pot2val;
+PushButton sw_center = PushButton(3, ENABLE_INTERNAL_PULLUP);
+PushButton sw_right = PushButton(4, ENABLE_INTERNAL_PULLUP);
+const int relay1 = 5;
+const int relay2 = 6;
 
-const int ledPin =  9;
-const long interval = 300; // blink interval
+const int mainLED =  8;
+const int relayLED = 7;
+const long blinkInterval = 300; // blink interval
 const int PCchannel = 13;
 const int CCchannel = 1;
+const int INchannel = 16;
 int currentProgram = 0;
-int ledState = HIGH;
+int mainLedState = HIGH;
+bool loopState = false;
 unsigned long previousMillis = 0;
 String mode = "CC"; // "CC" = Control Change, PC" = Program Change
 const int cc_left   = 0;
 const int cc_right  = 1;
 const int cc_center = 2;
-/*
-const int cc_pot1   = 3;
-const int cc_pot2   = 4;
-const int cc_pot3   = 5;
-const int cc_pot4   = 6;
-*/
-const int cc_pot[4] = {3, 4, 5, 6};
+const int cc_pot[POT_COUNT] = {3, 4, 5, 6};
+const int cc_relay = 7;
+const int pot[POT_COUNT] = {A0, A1, A2, A3};
+int potval[POT_COUNT];
+int potvalIN[POT_COUNT];
+bool potPosCorrect[POT_COUNT] = {true, true, true, true};
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 void setup() {
-        // MIDI baud rate
-        if(debug)
-          Serial.begin(9600);
-        else
-          Serial.begin(31250);
-        pinMode(ledPin, OUTPUT);
-        digitalWrite(ledPin, HIGH);
-        for(int i=0; i<4; i++) {
-          potval[i] = analogRead(pot[i]);
-        }
+  MIDI.begin(INchannel);
+  MIDI.turnThruOff();
+  // MIDI baud rate
+  if(debug)
+    Serial.begin(9600);
+  else
+    Serial.begin(31250);
+  // For brighter LEDs, uncomment these two lines:
+  //pinMode(mainLED, OUTPUT);
+  //pinMode(mainLED, OUTPUT);
+  digitalWrite(mainLED, HIGH);
+  for(int i=0; i<4; i++) {
+    potval[i] = analogRead(pot[i]);
+  }
 
-        // Footswitch press and release callbacks
-        sw_left.onPress(onButtonPressed);
-        sw_center.onPress(onButtonPressed);
-        sw_right.onPress(onButtonPressed);
+  // Footswitch press and release callbacks
+  sw_left.onPress(onButtonPressed);
+  sw_center.onPress(onButtonPressed);
+  sw_right.onPress(onButtonPressed);
 
-        sw_left.onRelease(onButtonReleased);
-        sw_center.onRelease(onButtonReleased);
-        sw_right.onRelease(onButtonReleased);
-        
-        // If center footswitch is been held for 1.5 seconds, switch to PC mode
-        sw_center.onHold(1500, setPCmode);
+  sw_left.onRelease(onButtonReleased);
+  sw_center.onRelease(onButtonReleased);
+  sw_right.onRelease(onButtonReleased);
+  
+  // If center footswitch is held for 1.5 seconds, switch to PC mode
+  sw_center.onHold(1500, setPCmode);
+  
+  // relay inputs:
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  switchLoop(false);
 }
 
 void loop() {
+  if (MIDI.read()) {
+    if (MIDI.getType() == midi::ControlChange) {
+      // If a CC that corresponds to one of the knobs is received,
+      // switch to "pick-up" mode by blocking sending MIDI CC 
+      // until the knob position and the controller value match.
+      int ccNumber = MIDI.getData1();
+      int index = isPotCC(ccNumber);
+      if( index >= 0 && pickUpMode) { // it is a knob CC
+        potvalIN[index] = MIDI.getData2();
+        if(abs( potvalIN[index] - potval[index] ) < 2)
+          potPosCorrect[index] = true;
+        else
+          potPosCorrect[index] = false;
+      } else {
+        if(ccNumber == cc_relay) {
+          loopState = MIDI.getData2() > 63;
+          switchLoop(loopState);
+        }
+      }
+    }
+  }
   sw_left.update();
   sw_right.update();
   sw_center.update();
   unsigned long currentMillis = millis();
   if(mode == "PC") {
-    if (currentMillis - previousMillis >= interval) {
+    if (currentMillis - previousMillis >= blinkInterval) {
       previousMillis = currentMillis;
-      ledState = (!ledState);
-      digitalWrite(ledPin, ledState);
+      mainLedState = (!mainLedState);
+      digitalWrite(mainLED, mainLedState);
     } 
   }
-  else // CC mode
-    digitalWrite(ledPin, HIGH);
 
   int potvalNew[4];
   for(int i=0; i<4; i++) {
@@ -83,70 +116,126 @@ void loop() {
   for(int i=0; i<4; i++) {
     if( abs(potval[i] - potvalNew[i]) > 7 && mode == "CC") {
       int outval = mapAndClamp(potval[i]);
-      MIDI.sendControlChange(cc_pot[i], outval, CCchannel);
       potval[i] = potvalNew[i];
-      debugControl("pot", i, potvalNew[i]);
+      if(potPosCorrect[i]) {
+        ccSend(cc_pot[i], outval, CCchannel);
+      } else {
+        // Check if knob position matches value received via MIDI.
+        // If so, allow sending MIDI again (i.e. pick-up).
+        if( abs( outval - potvalIN[i] ) < 2 ) {
+          potPosCorrect[i] = true;
+        }
+      }
     }
   }
 }
 
 void configurePushButton(Bounce& bouncedButton){
-        bouncedButton.interval(10); //10 is default
+  bouncedButton.interval(10); //10 is default
 }
 
 void onButtonPressed(Button& btn){
-        if(btn.is(sw_left)) {
-          if(mode == "PC") {
-            if(currentProgram > 0)
-              currentProgram--;
-            else
-              currentProgram = 63;
-            MIDI.sendProgramChange(currentProgram, PCchannel);
-          }
-          else
-            MIDI.sendControlChange(cc_left, 127, CCchannel);
-        } else if (btn.is(sw_right)){
-          if(mode == "PC") {
-            if(currentProgram < 63)
-              currentProgram++;
-            else
-              currentProgram = 0;
-            MIDI.sendProgramChange(currentProgram, PCchannel);
-          }
-          else
-            MIDI.sendControlChange(cc_right, 127, CCchannel);
-        } else if (btn.is(sw_center)) {
-          if(mode == "PC")
-            setCCmode();
-          else
-            MIDI.sendControlChange(cc_center, 127, CCchannel);
-        }
+  if(btn.is(sw_left)) {
+    if(mode == "PC") {
+      if(currentProgram > 0)
+        currentProgram--;
+      else
+        currentProgram = 63;
+      pcSend(currentProgram, PCchannel);
+    }
+    else
+      ccSend(cc_left, 127, CCchannel);
+  } else if (btn.is(sw_right)){
+    if(mode == "PC") {
+      if(currentProgram < 63)
+        currentProgram++;
+      else
+        currentProgram = 0;
+      pcSend(currentProgram, PCchannel);
+    }
+    else
+      ccSend(cc_right, 127, CCchannel);
+  } else if (btn.is(sw_center)) {
+    if(mode == "PC")
+      setCCmode();
+    else
+      ccSend(cc_center, 127, CCchannel);
+    if(loopInternal) {
+      loopState = !loopState;
+      switchLoop(loopState);
+    }
+  }
 }
 
 void onButtonReleased(Button& btn, uint16_t duration){
-        if(btn.is(sw_left)) {
-          if(mode == "CC")
-            MIDI.sendControlChange(cc_left, 0, CCchannel);
-        } else if (btn.is(sw_right)){
-          if(mode == "CC")
-            MIDI.sendControlChange(cc_right, 0, CCchannel);
-        } else if (btn.is(sw_center)) {
-          if(mode == "CC")
-            MIDI.sendControlChange(cc_center, 0, CCchannel);
-        }
+  if(btn.is(sw_left)) {
+    if(mode == "CC")
+      ccSend(cc_left, 0, CCchannel);
+  } else if (btn.is(sw_right)){
+    if(mode == "CC")
+      ccSend(cc_right, 0, CCchannel);
+  } else if (btn.is(sw_center)) {
+    if(mode == "CC")
+      ccSend(cc_center, 0, CCchannel);
+  }
 }
 
 void setCCmode() {
+  if(debug) Serial.println("CC mode");
   mode = "CC";
+  //else // CC mode
+  digitalWrite(mainLED, HIGH);
 }
 
 void setPCmode() {
-  MIDI.sendControlChange(cc_center, 0, CCchannel);
+  if(debug) Serial.println("PC mode");
+  ccSend(cc_center, 0, CCchannel);
   mode = "PC";
 }
 
-void debugControl(String name, int i, int value) {
-  if (debug) {
+void ccSend(int cc, int value, int channel) {
+  if(debug) {
+    debugThis("cc", cc, value);
+  } else {
+    if(srlMIDI) {
+      MIDI.sendControlChange(cc, value, channel);
+    }
+    if(usbMIDI) {
+      midiEventPacket_t event = {0x0B, 0xB0 | channel, cc, value};
+      MidiUSB.sendMIDI(event);
+      MidiUSB.flush();
+    }
+  }
+}
+
+void pcSend(int value, int channel) {
+  if(debug) {
+    debugThis("pc", -1, value);
+  } else {
+    if(srlMIDI) {
+      MIDI.sendProgramChange(value, channel);
+    }
+    if(usbMIDI) {
+      midiEventPacket_t event = {0x0B, 0x0C | channel, value, 0};
+      MidiUSB.sendMIDI(event);
+      MidiUSB.flush();
+    }
+  }
+}
+
+void switchLoop(bool state) {
+  if(debug) {
+    Serial.print("loop: ");
+    Serial.println(loopState);    
+  }
+  digitalWrite(relay1, !state); // Relays are open on high
+  digitalWrite(relay2, !state);
+  digitalWrite(relayLED, state);
+}
+
+
+void debugThis(String name, int i, int value) {
+  if(debug) {
     Serial.print(name);
     Serial.print("[");
     Serial.print(i);
@@ -154,13 +243,23 @@ void debugControl(String name, int i, int value) {
     Serial.print(": ");
     Serial.print(value);
     Serial.print("\tmapped: ");
-    Serial.println(mapAndClamp(value)); 
+    Serial.println( mapAndClamp(value) );
   }
 }
 
 int mapAndClamp(int input) {
-  int outval = map(input, 5, 1015, 0, 127);
+  int inMin = 5;
+  int inMax = 1014;
+  int outval = map(input, inMin, inMax, 0, 127);
   if(outval < 0) outval = 0;
   if(outval>127) outval = 127;
   return (outval);
+}
+
+int isPotCC(int val) {
+  for(int i=0; i<POT_COUNT; i++) {
+    if(cc_pot[i] == val)
+      return (i);
+  }
+  return (-1);
 }
